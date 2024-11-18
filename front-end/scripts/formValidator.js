5acea26cc88f123da5e3760ae2d96875441fb845
Luigi573 <106448901+Luigi573@users.noreply.github.com>
2024-11-18 12:39:50 -0600

    UpdateContactForm
    Add validation and restructured fields to align them all

--- front-end/scripts/formValidator.js
+++ front-end/scripts/formValidator.js
@@ -1,18 +1,50 @@
-$(document).ready(function () {
-    $('#contactForm').validate({
-        rules: {
-            inputEmail: {
-                required: true,
-                email: true
-            }
+$('#contactForm').validate({
+    rules: {
+        inputName:{
+            required: true,
+            minlength: 15,
+            maxlength: 50
         },
-        messages: {
-            inputEmail: {
-                required: "Este campo es obligatorio.",
-                email: "Ingrese un correo válido."
-            }
+        inputEmail:{
+            required: true,
+            email: true
         },
-        errorClass: 'is-invalid',
-        validClass: 'is-valid'
-    });
+        inputPhone:{
+            required: true,
+            minlength: 10,
+            maxlength: 10
+        },
+        inputService:{
+            required: true
+        },
+        inputRequestBody:{
+            required: true
+        }
+    },
+    messages: {
+        inputName: {
+            required: "Este campo es obligatorio.",
+            minlength: "El nombre debe tener al menos 15 caracteres.",
+            maxlength: "El nombre no puede exceder 50 caracteres"
+        },
+        inputEmail: {
+            required: "Este campo es obligatorio.",
+            email: "Ingrese un correo válido."
+        },
+        inputPhone: {
+            required: "Este campo es obligatorio.",
+            minlength: "El teléfono debe tener 10 caracteres.",
+            maxlength: "El teléfono debe tener 10 caracteres."
+        },
+        inputService: {
+            required: "Debe seleccionar un servicio."
+        },
+        inputRequestBody: {
+            required: "Por favor ingrese su solicitud."
+        }
+    },
+    
+    submitHandler: function(form){
+        form.submit();
+    }
 });
